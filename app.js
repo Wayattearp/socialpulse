9ae1f96d6e376a1b8b84dd6ecccb889f77560ee0
Wayattearp <iason148348@gmail.com>
2023-10-12 13:55:30 -0400

    Add user registration

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ mongoose
         console.error("MongoDB connection error:", err);
     });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/", (req, res) => res.send("Hello world"));
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`))
