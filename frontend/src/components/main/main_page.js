e85fba2b084b7ab131a6bc834e8069ed6970da50
Wayattearp <iason148348@gmail.com>
2023-10-18 12:51:15 -0400

    Add main page

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>A Twitter Clone</h1>
+                <footer>
+                    Footer
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
